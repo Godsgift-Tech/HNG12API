a207be67086a421a48ee051c9c390d81f3495ff4
Godsgift Oghenechohwo <godsgiftoghenechohwo@gmail.com>
2025-01-31 08:09:03 +0100

    Add project files.

---
+++ Controllers/InformationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNG12API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InformationController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult GetInformation()
+        {
+            var response = new
+            {
+                email = "godsgiftoghenechohwo@gmail.com",
+                currentDate = DateTime.UtcNow.ToString("o"),
+                githubUrl = ""
+
+                //return Ok("Welcome to HNG12 API");
+            };
+            return Ok(response);    
+        }
+    }
+}
